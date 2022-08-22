fc13c1ce25a8951ea4b2a661c8a02f69e046b956
andi-lestianto <andi7les@gmail.com>
2022-08-22 14:24:42 +0700

    getstaticprops

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -4,12 +4,10 @@ import { useRouter } from "next/router";
 export default function detail() {
   const router = useRouter();
   const id = router.query;
-  const yourid = id.id
   console.log(id)
-  console.log('Anda di halaman ' + yourid)
 
 
   return (
-    <p>Detail Page {id.id}</p>
+    <p>Anda di halaman {id.id}</p>
   )
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,39 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
+import styles from "../../styles/Users.module.css";
 
-export default function index() {
+interface UserData {
+  dataUsers: Array<any>;
+}
+
+export default function Users(props: UserData) {
+  const route = useRouter();
+  const { dataUsers } = props;
+  console.log(dataUsers)
+
+
   return (
     <Layout pageTitle="User Page" >
+
       <p>User Page</p>
+      {dataUsers.map((user) => (
+        <div className={styles['card-user']} onClick={() => route.push(`/users/${user.name}`)}>
+          <div className={styles['card-item']}>{user.name} {user.email}</div>
+        </div>
+      ))}
+
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers
+    }
+  }
+
 }
