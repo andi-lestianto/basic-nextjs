b12f1a5d82325f09d62b3c80e21b903c5687c6eb
andi-lestianto <andi7les@gmail.com>
2022-08-22 17:00:58 +0700

    axios init

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,34 +1,58 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
-export default function BlogDetail(props) {
-    const { postDetail } = props;
-    console.log(postDetail);
+export default function BlogDetail() {
+    // const { postDetail } = props;
+    // console.log(postDetail);
+
+    const [postData, setPostData] = useState([]);
 
     const router = useRouter();
     const query = router.query;
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`)
+        .then(function (response) {
+            console.log(response.data.body)
+            setPostData(response.data)
+        })
+        .catch(function (error) {
+            console.log(error)
+            console.log(error.status)
+        });
 
-    // axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`).then(response => {
-    //     console.log(response.data.body)
-    // })
+    // useEffect(() => {
+    //     axios.get(`${base_url}/produk/${query.id}`)
+    //         .then(response => {
+    //             let data = response.data.data;
+    //             console.log(data);
+    //             setProduct(data);
+    //             setProductGallery(response.data.gallery);
+    //             //set first image
+    //             showImage(99, data.gambar)
+    //             return data;
+    //         }).catch((error) => {
+    //             console.log('error ' + error);
+    //         });
+    // }, [])
+
+
 
     return (
-        <div>id</div>
+        <div></div>
     )
 
 }
-export async function getServerSideProps() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + 1);
-    const postDetail = await res.json();
+// export async function getServerSideProps() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + 1);
+//     const postDetail = await res.json();
 
-    return {
-        props: {
-            postDetail,
-        }
-    }
-}
+//     return {
+//         props: {
+//             postDetail,
+//         }
+//     }
+// }
 
 
 
